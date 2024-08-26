cf9599337bbd6dcbbaa9695fe620dfea165c4935
Lunar-Dawn <lunarrequiem42@gmail.com>
2024-08-26 15:59:54 +0200

    Major refactor, adding copy to clipboard functionality to each card.

--- yaktribe-tts-description-exporter.user.js
+++ yaktribe-tts-description-exporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YakTribe TTS description exporter
 // @namespace    lunarrequiem.net
-// @version      0.0.2
+// @version      0.1.0
 // @description  Exports Necromunda Underhive data to a format that can be pasted into TTS model descriptions
 // @author       Lunar Dawn
 // @match        https://yaktribe.games/underhive/print/cards/*
@@ -13,6 +13,67 @@
 // @updateURL    https://raw.githubusercontent.com/Lunar-Dawn/Yaktribe-TTS-Export/main/yaktribe-tts-description-exporter.user.js
 // @downloadURL  https://raw.githubusercontent.com/Lunar-Dawn/Yaktribe-TTS-Export/main/yaktribe-tts-description-exporter.user.js
 // ==/UserScript==
+
+const styleTag = `
+<style>
+    .export-all-button {
+        position: fixed;
+        top: 0;
+        right: 0;
+        background: #06BE00;
+        border: 0;
+        padding: 15px;
+        font-size: 1.4em;
+        border-left: grey 1px solid;
+        border-bottom: grey 1px solid;
+        border-bottom-left-radius: 5px;
+    }
+    .export-all-button:hover {
+        background: #07DC00;
+    }
+
+    .card-toolbar {
+        display: flex;
+        opacity: 0;
+
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+
+        border-top: 1px #636363 solid;
+
+        transition: opacity 1s;
+    }
+    .gang-ganger-card:hover > .card-toolbar {
+        opacity: 1;
+        transition: opacity 0s;
+    }
+
+    .card-toolbar > button {
+        padding: 10px 0;
+        flex: 1;
+        border: 0;
+        background: #CCCCCC;
+
+        transition: background 0.1s;
+    }
+    .card-toolbar > button:hover {
+        background: #AAAAAA;
+
+        transition: background 0s;
+    }
+    .card-toolbar > button.last-pressed-button {
+        background: #07DC00;
+        transition: background 0.1s;
+    }
+    .card-toolbar > button.last-pressed-button:hover {
+        background: #07DC00;
+    }
+    .force-visible {
+        opacity: 1;
+    }
+</style>`;
 
 class Weapon {
     constructor(tableRow) {
@@ -64,9 +125,11 @@ class Ganger {
         this.rules = finalTable.has("td:contains('RULES')")?.find("td")[1]?.innerText?.split(",").map(s => s.trim()).filter(Boolean) || ["-"]
     }
 
-    toText() {
-        return `[56f442]${this.W}/${this.W}[-] [f51105]${this.Name}[-] [edf505](${this.Type})[-]\n` +
-            `\n` +
+    get modelName() {
+        return `[56f442]${this.W}/${this.W}[-] [f51105]${this.Name}[-] [edf505](${this.Type})[-]`
+    }
+    get modelDescription() {
+        return `` +
             `[56f442]M  WS  BS   S   T   W   I    A   Ld  Cl  Wil  Int[-]\n` +
             `${this.M}   ${this.WS}   ${this.BS}  ${this.S}   ${this.T}   ${this.W}   ${this.I}  ${this.A}   ${this.Ld}  ${this.Cl}  ${this.Wil}  ${this.Int}\n` +
             `\n` +
@@ -80,49 +143,87 @@ class Ganger {
             `${this.skills.join(', ')}\n` +
             `\n` +
             `[dc61ed]Special Rules[-]\n` +
-            `${this.rules.join('\n')}\n`
+            `${this.rules.join('\n')}`
+    }
+
+    toBlob() {
+        return new Blob([`${this.modelName}\n\n${this.modelDescription}\n`])
+    }
+    saveFile() {
+        saveAs(this.toBlob(), `${this.Name}.txt`)
     }
 }
 
-const exportGangs = () => {
-    const gangers = $(".gang-ganger-card")
+// This *currently* isn't slow enough to warrant being made async, but the code would allow it
+const gangers = $(".gang-ganger-card")
         .slice(0, -1)
         .toArray()
         .map(e => new Ganger(e))
 
+const saveAll = () => {
     const zip = new JSZip()
 
-    gangers.forEach(g => zip.file(`${g.Name}.txt`, g.toText()))
+    gangers.forEach(g => zip.file(`${g.Name}.txt`, g.toBlob()))
     zip.generateAsync({type:"blob"})
         .then(b => saveAs(b, "export.zip"))
 }
 
-const styleTag = `
-<style>
-.export-button {
-    position: fixed;
-    top: 0;
-    right: 0;
-    background: #06BE00;
-    border: 0;
-    padding: 15px;
-    font-size: 1.4em;
-    border-left: grey 1px solid;
-    border-bottom: grey 1px solid;
-    border-bottom-left-radius: 5px;
+function markLastClicked(e) {
+    const button = $(this)
+    const span = button.find('span')
+
+    span
+        .fadeOut(100, () => span.text('✔️ Copied').fadeIn(100))
+
+    button
+        .addClass('last-pressed-button')
+        .parent()
+        .addClass('force-visible')
+
+    setTimeout(() => {
+        button
+            .removeClass('last-pressed-button')
+            .parent()
+            .removeClass('force-visible')
+        span
+            .fadeOut(100, () => span.text(e.data.oldText).fadeIn(100))
+    }, 2000)
 }
-.export-button:hover {
-    background: #07DC00;
+
+const createCardToolbar = i => {
+    const toolbar = $(`<div>`)
+        .addClass('card-toolbar')
+
+    $('<button><span>Copy Name</span></button>')
+        .on('click', () => navigator.clipboard.writeText(gangers[i].modelName))
+        .on('click', { oldText: 'Copy Name' }, markLastClicked)
+        .appendTo(toolbar)
+
+    $('<button><span>Copy Description</span></button>')
+        .on('click', () => navigator.clipboard.writeText(gangers[i].modelDescription))
+        .on('click', { oldText: 'Copy Description' }, markLastClicked)
+        .css("border-right", "1px #636363 solid")
+        .css("border-left", "1px #636363 solid")
+        .appendTo(toolbar)
+
+    $('<button>')
+        .text('Download')
+        .on('click', () => gangers[i].saveFile())
+        .appendTo(toolbar)
+
+    return toolbar
 }
-</style>`;
 
 (function() {
     'use strict';
 
     const button = $("<button>Export all as .txt files</button>")
-        .addClass('export-button')
-        .click(exportGangs)
+        .addClass('export-all-button')
+        .on('click', saveAll)
 
     $('head').append(styleTag)
     $('body').append(button)
+    $(".gang-ganger-card")
+        .slice(0, -1)
+        .append(createCardToolbar)
 })();
